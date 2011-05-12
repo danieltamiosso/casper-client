78a4b42b42d04058afac8f4fbe0deb75f4e0fd4b
Daniel Tamiosso <email@danieltamiosso.com>
2011-05-11 22:36:45 -0300

    Refactoring to post using json as content type

--- spec/casper_client_spec.rb
+++ spec/casper_client_spec.rb
@@ -1,42 +1,64 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 
 describe Casper::Client do
-  
-  TEMPLATE = open(File.join(File.dirname(__FILE__), 'data', 'report.jrxml'))
-  DATASET = open(File.join(File.dirname(__FILE__), 'data','dataset.xml'))
-    
+      
   describe 'a basic flow of generation of a report' do
     
     before(:each) do
       RestClient.stub!(:post).and_return('report')
     end
 
-    it 'should build the correct request to a report with a xml datasource' do
+    it 'should build the correct request to a report with a xml file as datasource' do
       RestClient.should_receive(:post).with(
         'http://casper.jrs-labs.com:8080',
-        {'casper[jrxml]'=> TEMPLATE, 
-          'casper[data]'=> DATASET, 
-          'casper[xpath]'=>'//root'
-        }
+        {:casper =>
+              {
+                :jrxml => Base64.encode64(open(File.join(File.dirname(__FILE__),'data/report.jrxml')).read),
+                :data => Base64.encode64(open(File.join(File.dirname(__FILE__),'data/dataset.xml')).read),
+                :xpath => '//root'
+              }
+        }.to_json, :content_type => :json, :accept => :json
       )
       report = Casper::Client.build do |report|
-        report.xml = DATASET
-        report.template = TEMPLATE
+        report.template = open(File.join(File.dirname(__FILE__),'data/report.jrxml'))
+        report.xml = open(File.join(File.dirname(__FILE__),'data/dataset.xml'))
+        report.xpath = '//root'
+      end
+    end
+
+    it 'should build the correct request to a report with a xml file as datasource with a optional host' do
+      RestClient.should_receive(:post).with(
+        'http://myserver',
+        {:casper =>
+              {
+                :jrxml => Base64.encode64(open(File.join(File.dirname(__FILE__),'data/report.jrxml')).read),
+                :data => Base64.encode64(open(File.join(File.dirname(__FILE__),'data/dataset.xml')).read),
+                :xpath => '//root'
+              }
+        }.to_json, :content_type => :json, :accept => :json
+      )
+      report = Casper::Client.build :host => 'http://myserver' do |report|
+        report.template = open(File.join(File.dirname(__FILE__),'data/report.jrxml'))
+        report.xml = open(File.join(File.dirname(__FILE__),'data/dataset.xml'))        
         report.xpath = '//root'
       end
     end
     
-    it 'should build the correct request to a report with a xml datasource with a optional host' do
+ 
+    it 'should build the correct request to a report with a xml string as datasource' do
       RestClient.should_receive(:post).with(
         'http://mycasperserver.com',
-        {'casper[jrxml]'=> TEMPLATE, 
-          'casper[data]'=> DATASET, 
-          'casper[xpath]'=> '//root'
-        }
+        {:casper =>
+              {
+                :jrxml => Base64.encode64(open(File.join(File.dirname(__FILE__),'data/report.jrxml')).read),
+                :data => Base64.encode64('<model/>'),
+                :xpath => '//root'
+              }
+        }.to_json, :content_type => :json, :accept => :json
       )
       report = Casper::Client.build :host => 'http://mycasperserver.com' do |report|
-        report.xml = DATASET
-        report.template = TEMPLATE
+        report.template = open(File.join(File.dirname(__FILE__),'data/report.jrxml'))
+        report.xml = '<model/>'
         report.xpath = '//root'
       end
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,1 @@
-require File.join(File.dirname(__FILE__), '..','lib','casper_client')
+require File.join(File.dirname(__FILE__), '..','lib','casper-client')

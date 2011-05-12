6249e50a2ea7bc38ad4b5785be6302bc3d6db8cc
Daniel Tamiosso <email@danieltamiosso.com>
2011-05-11 22:34:50 -0300

    Refactoring to post using json as content type

--- lib/casper-client.rb
+++ lib/casper-client.rb
@@ -1,4 +1,6 @@
 require 'rest_client'
+require 'json'
+require 'base64'
 
 module Casper
 
@@ -10,13 +12,24 @@ module Casper
       options = {:host => 'http://casper.jrs-labs.com:8080'}.merge!(options)
       report = Report.new
       report.instance_eval(&block)
-      RestClient.post options[:host], {'casper[jrxml]' => report.template, 'casper[data]' => report.xml, 'casper[xpath]' => report.xpath}
+      RestClient.post options[:host], report.to_json, :content_type => :json, :accept => :json
     end
   
   end
   
   class Report
       attr_accessor :xml, :template, :xpath
+      
+      def to_json
+        {:casper =>
+              {
+                :jrxml => Base64.encode64(self.template.read),
+                :data => Base64.encode64((self.xml.kind_of? File) ? self.xml.read : xml),
+                :xpath => self.xpath
+              }
+        }.to_json
+      end
+      
   end
 
 end
